ad7ef7e6e4d2b574afae0d078c8e2ddd60cce6fe
Eran Peled <eranltd@gmail.com>
2019-11-08 11:02:06 +0200

    Custom-Form-Implementation

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React from 'react';
 
-import "./sign-in.styles.scss";
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: "",
-      password: ""
+      email: '',
+      password: ''
     };
   }
+
   handleSubmit = event => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    this.setState({ email: '', password: '' });
   };
 
   handleChange = event => {
@@ -25,20 +29,30 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className="sign-in">
+      <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-       
-        <input name="email" type="email" value={this.state.email} onChange={this.handleChange} required/>
-        <label>Email</label>
-        <input name="password" type="password" value={this.state.password} required/>
-        <input type="submit" value="Submit"/>
-
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <CustomButton type='submit'> Sign in </CustomButton>
         </form>
-      
-        </div>
+      </div>
     );
   }
 }
