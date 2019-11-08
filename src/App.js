2233e1a90e07eeccaecdda69c8a75d9cede83bd7
Eran Peled <eranltd@gmail.com>
2019-11-08 10:46:43 +0200

    sign-in component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Login from './pages/login/login.component';
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div>
+    <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/login' component={Login} />
+
       </Switch>
     </div>
   );
---
+++ src/pages/login/login.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './login.styles.scss'
+
+
+const Login = () => (
+    <div className="login">
+    <SignIn />
+
+    </div>
+);
+
+export default Login;
+
+
