492353893553ce73c73ce0c75476c086f7ff5e24
Eran Peled <eranltd@gmail.com>
2019-11-08 08:17:21 +0200

    ShopPage

---
+++ src/components/shop/shop.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SHOP_DATA from './shop.data.js'
+
+
+
+class ShopPage extends React.Component{
+
+    constructor(props){
+        super(props);
+
+
+        this.state = {
+            Collection: SHOP_DATA
+        }
+    }
+
+    render(){
+        return <div>SHOP PAGE</div>
+    }
+
+}
+
+export default ShopPage;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './homepage.styles.scss';
+
 import Directory from '../../components/directory/directory.component';
 
-const HomePage = () => (
+import './homepage.styles.scss';
 
-    <div className='homepage'>
-   <Directory></Directory>
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
   </div>
 );
 
